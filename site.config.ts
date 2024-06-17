75c796d9dbd351ed58a0117abb1048330fbb1e18
Gaurav Kumar <84905312+GauravKumar9920@users.noreply.github.com>
2024-06-18 02:16:52 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,9 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: 'GauravKumar9920',
+  linkedin: 'gaurav-kumar-b7298220a',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
